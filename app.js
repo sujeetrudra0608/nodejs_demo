b992c0dfe1dc42499a82a9e6e0b8f4744cd14445
Your Name <you@example.com>
2024-01-02 15:50:26 +0530

    Node.js Course - 01 - Create Server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Sujeet')
+//create a server listen on port no 9000
+const http = require('http')
+
+const PORT = 9000
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: 'Hello World!',
+  }));
+})
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
